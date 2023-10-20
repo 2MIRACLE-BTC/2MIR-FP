c9644a4c7d3635429b5d1de5169f3682a5455810
minsu choi <zxy0575@naver.com>
2023-10-20 21:08:52 +0900

    edit k8sSetup.sh

--- k8s_setup/k8sSetup.sh
+++ k8s_setup/k8sSetup.sh
@@ -13,19 +13,26 @@ sudo sh -c 'cat >> /etc/hosts <<EOF
 EOF'
 
 ### [공통 작업] k8s setup dir 생성 후 관련 파일 생성
-mkdir ~/k8sSetup; cd ~/k8sSetup
+echo "cp *.sh ~/k8sSetup"
+mkdir ~/k8sSetup;
+find . -maxdepth 1 -type f -name "*.sh" ! -name "k8sSetup.sh" -exec cp {} ~/k8sSetup/ \;
 
 ### [공통 작업] *.sh 실행
-#echo "create iptableset.sh"
-#sh iptableset.sh
+echo "create iptableset.sh"
+sh ~/k8sSetup/iptableset.sh
+sh ~/k8sSetup/dockercreate.sh
+sh ~/k8sSetup/installkubeadm.sh
+
 
 ###### 노드 역할에 따른 *.sh 설치 진행
 if [ "$hostname" = "K8S-MASTER" ]; then
+    # 마스터 노드 진행
     sudo hostnamectl hostname K8S-MASTER
-    #sh kubeadminit.sh
-	#sh calicosetup.sh
+    sh kubeadminit.sh
+	sh calicosetup.sh
 else
-    #sudo hostnamectl hostname K8S-WORKER$myPriIP
+    # 워커 노드 진행
+    echo "마스터 노드에서 생성된 kubeadm join 실행 필요"
 fi
 
 
