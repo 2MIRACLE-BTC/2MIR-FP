6a631eb26c563ddc442f03cb07cd5fa6b462510f
minsu choi <zxy0575@naver.com>
2023-10-21 19:28:39 +0900

    CMS_edit k8s

--- k8s_setup/kubeadminit.sh
+++ k8s_setup/kubeadminit.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
 sudo kubeadm init --token-ttl 0 --pod-network-cidr=192.168.0.0/16 --cri-socket unix:///var/run/containerd/containerd.sock >> kubeadm-init-result.txt
 
+while true; do
+    if grep -q "Your Kubernetes control-plane has initialized successfully" kubeadm-init-result.txt; then
+        echo "kubeadm init completed successfully"
+        break
+    else
+        echo "Waiting for kubeadm init to complete..."
+        sleep 10
+    fi
+done
 
 cat kubeadm-init-result.txt | tail -10 > k8s-token.txt
 
-sudo rm kubeadm-init-result.txt
-
+sudo rm -f kubeadm-init-result.txt
 
 mkdir -p $HOME/.kube
 sudo rm -f $HOME/.kube/config
