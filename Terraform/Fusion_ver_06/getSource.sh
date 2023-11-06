a981d14125569b24017895f3d5ee05384fbb871e
cms5092 <112603478+cms5092@users.noreply.github.com>
2023-11-06 11:48:50 +0900

    Update getSource.sh

--- Terraform/Fusion_ver_06/getSource.sh
+++ Terraform/Fusion_ver_06/getSource.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ### <초기화>
+#sed -i "/TF_STATE_S3/c\     TF_STATE_S3  = \"default\"" ./terragrunt.hcl
+#sed -i "/TF_DYNAMO_DB/c\     TF_DYNAMO_DB = \"default\"" ./terragrunt.hcl
 sed -i '/bucket  = /c\    bucket  = "example_TF_S3"' ./terragrunt.hcl
+sed -i '/bucket  = /c\        bucket  = "example_TF_S3"' ./DEV/DEV_EKS/terragrunt.hcl
 sed -i '/dynamodb_table = /c\    dynamodb_table = "example_TF_DY"' ./terragrunt.hcl
 ### </초기화>
 
@@ -9,6 +12,7 @@ sed -i '/dynamodb_table = /c\    dynamodb_table = "example_TF_DY"' ./terragrunt.
 echo "Insert TF_STATE_S3 name"
 read TF_STATE_S3
 sed -i "s/example_TF_S3/$TF_STATE_S3/g" ./terragrunt.hcl
+sed -i "s/example_TF_S3/$TF_STATE_S3/g" ./DEV/DEV_EKS/terragrunt.hcl
 
 echo "Insert TF_DYNAMO_DB name"
 read TF_DYNAMO_DB
